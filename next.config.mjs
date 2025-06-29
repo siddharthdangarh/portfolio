08b382eb0731f9890f901087eee86aacc625379c
sidsaara <siddharth.d@saara.io>
2025-06-29 17:57:53 +0530

    init: portfolio

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Enables static site export
+    images: {
+        unoptimized: true, // Required for static exports if using next/image
+            remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ], // for external images
+    },
+    trailingSlash: true, // Ensures proper routing for GitHub Pages
+};
+    
+export default nextConfig;
+    
